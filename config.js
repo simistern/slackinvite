b9c0f45f5d67ba92dab476b893500c471bac45db
Simon <simon160@gmail.com>
2016-02-21 16:00:54 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // your community or team name to display on join page.
+  community: process.env.COMMUNITY_NAME || 'circleacademy',
+  // your slack team url (ex: socketio.slack.com)
+  slackUrl: process.env.SLACK_URL || 'circleacademy.slack.com',
+  // access token of slack
+  // You can generate it in https://api.slack.com/web#auth
+  // You should generate the token in admin user, not owner.
+  // If you generate the token in owner user, missing_scope error will be occurred.
+  //
+  // You can test your token via curl:
+  //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
+  //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
+  //   --compressed
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-22405128675-22408395159-22409645408-4faf2b96ea',
+
+  // an optional security measure - if it is set, then that token will be required to get invited.
+  inviteToken: process.env.INVITE_TOKEN || null
+};
